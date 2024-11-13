99e6db7ff2f305350b4e665d0b508cd26171778b
crisper2001 <crisperohman@gmail.com>
2024-11-12 21:38:08 -0300

    Commit inicial

---
+++ src/main/java/br/aluno/ueg/memo_io_back/MemoIoBackApplication.java
@@ -0,0 +1,13 @@
+package br.aluno.ueg.memo_io_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoIoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoIoBackApplication.class, args);
+	}
+
+}
