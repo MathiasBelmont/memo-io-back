15d23fdd575a96ebb26f8419c36f7baab39b6b75
Isaque <crisperohman@gmail.com>
2024-11-21 21:58:55 -0300

    ADD: Criação do 'NoteController' e do 'UserController'

---
+++ src/main/java/br/aluno/ueg/memo_io_back/controllers/NoteController.java
@@ -0,0 +1,46 @@
+package br.aluno.ueg.memo_io_back.controllers;
+
+import br.aluno.ueg.memo_io_back.mappers.NoteMapper;
+import br.aluno.ueg.memo_io_back.models.NoteModel;
+import br.aluno.ueg.memo_io_back.models.dtos.NoteCreateDTO;
+import br.aluno.ueg.memo_io_back.models.dtos.NoteUpdateDTO;
+import br.aluno.ueg.memo_io_back.services.NoteService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "${api.version}/memo-io-back/note")
+public class NoteController {
+
+    @Autowired
+    private NoteService service;
+
+    @Autowired
+    private NoteMapper mapper;
+
+    @PostMapping
+    @Operation(description = "Endpoint para adicionar notas")
+    public ResponseEntity<Object> create(@Valid @RequestBody NoteCreateDTO dto) {
+        try {
+            NoteModel model = service.create(mapper.toModel(dto));
+            return ResponseEntity.ok(model);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping
+    @Operation(description = "Endpoint para listar todas as notas")
+    public ResponseEntity<Object> getAll() {
+        try {
+            return ResponseEntity.of(Optional.ofNullable(service.getAll()));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/br/aluno/ueg/memo_io_back/controllers/UserController.java
@@ -0,0 +1,45 @@
+package br.aluno.ueg.memo_io_back.controllers;
+
+import br.aluno.ueg.memo_io_back.mappers.UserMapper; // Assuming you have a UserMapper
+import br.aluno.ueg.memo_io_back.models.UserModel; // Changed to UserModel
+import br.aluno.ueg.memo_io_back.models.dtos.UserDTO;
+import br.aluno.ueg.memo_io_back.services.UserService; // Changed to UserService
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "${api.version}/memo-io-back/user")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @Autowired
+    private UserMapper mapper;
+
+    @PostMapping
+    @Operation(description = "Endpoint para adicionar usuários")
+    public ResponseEntity<Object> create(@Valid @RequestBody UserDTO dto) {
+        try {
+            UserModel model = service.create(mapper.toModel(dto));
+            return ResponseEntity.ok(model);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping
+    @Operation(description = "Endpoint para listar todos os usuários")
+    public ResponseEntity<Object> getAll() {
+        try {
+            return ResponseEntity.of(Optional.ofNullable(service.getAll()));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
