b600df30ceb31dfdfb19a0f1adb5eeef8c35888a
Isaque <crisperohman@gmail.com>
2024-11-23 21:42:26 +0000

    MOD: Controladores atualizados

--- src/main/java/br/aluno/ueg/memo_io_back/controllers/NoteController.java
+++ src/main/java/br/aluno/ueg/memo_io_back/controllers/NoteController.java
@@ -2,35 +2,47 @@ package br.aluno.ueg.memo_io_back.controllers;
 
 import br.aluno.ueg.memo_io_back.mappers.NoteMapper;
 import br.aluno.ueg.memo_io_back.models.NoteModel;
+import br.aluno.ueg.memo_io_back.models.UserModel;
 import br.aluno.ueg.memo_io_back.models.dtos.NoteCreateDTO;
 import br.aluno.ueg.memo_io_back.models.dtos.NoteUpdateDTO;
 import br.aluno.ueg.memo_io_back.services.NoteService;
+import br.aluno.ueg.memo_io_back.services.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/memo-io-back/note")
 public class NoteController {
 
     @Autowired
-    private NoteService service;
+    private NoteService noteService;
 
     @Autowired
-    private NoteMapper mapper;
+    private UserService userService;
+
+    @Autowired
+    private NoteMapper noteMapper;
 
     @PostMapping
-    @Operation(description = "Endpoint para adicionar notas")
-    public ResponseEntity<Object> create(@Valid @RequestBody NoteCreateDTO dto) {
+    @Operation(description = "Endpoint para adicionar uma nota")
+    public ResponseEntity<Object> create(@Valid @RequestBody NoteCreateDTO noteCreateDTO) {
         try {
-            NoteModel model = service.create(mapper.toModel(dto));
-            return ResponseEntity.ok(model);
+            Optional<UserModel> author = userService.validateUserExists(noteCreateDTO.getAuthorId());
+            if (author.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("");
+            }
+            NoteModel note = noteService.create(noteMapper.toNoteModel(noteCreateDTO));
+            return ResponseEntity.status(HttpStatus.CREATED).body(note);
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
 
@@ -38,9 +50,59 @@ public class NoteController {
     @Operation(description = "Endpoint para listar todas as notas")
     public ResponseEntity<Object> getAll() {
         try {
-            return ResponseEntity.of(Optional.ofNullable(service.getAll()));
+            List<NoteModel> notes = noteService.getAll();
+            if (notes == null || notes.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("");
+            }
+            return ResponseEntity.ok(notes);
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    @Operation(description = "Endpoint para exibir os dados de uma nota pelo ID")
+    public ResponseEntity<Object> getById(@PathVariable Long id) {
+        try {
+            Optional<NoteModel> note = noteService.getById(id);
+            if (note.isPresent()) {
+                return ResponseEntity.ok(note);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    @Operation(description = "Endpoint para atualizar os dados de uma nota pelo ID")
+    public ResponseEntity<Object> updateById(@PathVariable Long id, @Valid @RequestBody NoteUpdateDTO noteUpdateDTO) {
+        try {
+            Optional<NoteModel> note = noteService.updateById(id, noteMapper.toNoteModel(noteUpdateDTO));
+            if (note.isPresent()) {
+                return ResponseEntity.ok(note);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(description = "Endpoint para deletar uma nota pelo ID")
+    public ResponseEntity<Object> deleteById(@PathVariable("id") Long id) {
+        try {
+            Optional<NoteModel> note = noteService.deleteById(id);
+            if (note.isPresent()) {
+                return ResponseEntity.ok(note);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
     }
 }
+
--- src/main/java/br/aluno/ueg/memo_io_back/controllers/UserController.java
+++ src/main/java/br/aluno/ueg/memo_io_back/controllers/UserController.java
@@ -2,14 +2,17 @@ package br.aluno.ueg.memo_io_back.controllers;
 
 import br.aluno.ueg.memo_io_back.mappers.UserMapper;
 import br.aluno.ueg.memo_io_back.models.UserModel;
-import br.aluno.ueg.memo_io_back.models.dtos.UserDTO;
+import br.aluno.ueg.memo_io_back.models.dtos.UserCreateDTO;
+import br.aluno.ueg.memo_io_back.models.dtos.UserUpdateDTO;
 import br.aluno.ueg.memo_io_back.services.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -17,19 +20,19 @@ import java.util.Optional;
 public class UserController {
 
     @Autowired
-    private UserService service;
+    private UserService userService;
 
     @Autowired
-    private UserMapper mapper;
+    private UserMapper userMapper;
 
     @PostMapping
-    @Operation(description = "Endpoint para adicionar usuários")
-    public ResponseEntity<Object> create(@Valid @RequestBody UserDTO dto) {
+    @Operation(description = "Endpoint para adicionar um usuário")
+    public ResponseEntity<Object> create(@Valid @RequestBody UserCreateDTO userCreateDTO) {
         try {
-            UserModel model = service.create(mapper.toModel(dto));
-            return ResponseEntity.ok(model);
+            UserModel user = userService.create(userMapper.toUserModel(userCreateDTO));
+            return ResponseEntity.status(HttpStatus.CREATED).body(user);
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
 
@@ -37,9 +40,58 @@ public class UserController {
     @Operation(description = "Endpoint para listar todos os usuários")
     public ResponseEntity<Object> getAll() {
         try {
-            return ResponseEntity.of(Optional.ofNullable(service.getAll()));
+            List<UserModel> users = userService.getAll();
+            if (users == null || users.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("");
+            }
+            return ResponseEntity.ok(users);
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    @Operation(description = "Endpoint para exibir os dados de um usuário pelo ID")
+    public ResponseEntity<Object> getById(@PathVariable Long id) {
+        try {
+            Optional<UserModel> user = userService.getById(id);
+            if (user.isPresent()) {
+                return ResponseEntity.ok(user);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    @Operation(description = "Endpoint para atualizar os dados de um usuário pelo ID")
+    public ResponseEntity<Object> updateById(@PathVariable Long id, @Valid @RequestBody UserUpdateDTO userUpdateDTO) {
+        try {
+            Optional<UserModel> user = userService.updateById(id, userMapper.toUserModel(userUpdateDTO));
+            if (user.isPresent()) {
+                return ResponseEntity.ok(user);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(description = "Endpoint para deletar um usuário pelo ID")
+    public ResponseEntity<Object> deleteById(@PathVariable("id") long id) {
+        try {
+            Optional<UserModel> user = userService.deleteById(id);
+            if (user.isPresent()) {
+                return ResponseEntity.ok(user);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
     }
 }
