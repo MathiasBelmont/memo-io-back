4d86a5f3845217de635c16a9111d259e39289ce8
Isaque <crisperohman@gmail.com>
2024-11-27 00:46:09 +0000

    ADD: Método 'findByEmail' em UserRepository

--- src/main/java/br/aluno/ueg/memo_io_back/repositories/UserRepository.java
+++ src/main/java/br/aluno/ueg/memo_io_back/repositories/UserRepository.java
@@ -1,9 +1,13 @@
 package br.aluno.ueg.memo_io_back.repositories;
 
 import br.aluno.ueg.memo_io_back.models.UserModel;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, Long> {
+    Optional<UserModel> findByEmail(String email);
 }
